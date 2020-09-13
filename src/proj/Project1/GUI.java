dbe2d2afe1bd3ad8b0d68d218d743abe44de8457
Yael Brown <yaelrbrown@gmail.com>
2020-09-13 01:15:34 -0500

    Figured out gui

--- src/proj/Project1/GUI.java
+++ src/proj/Project1/GUI.java
@@ -1,6 +1,7 @@
 package proj.Project1;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -8,20 +9,19 @@ public class GUI {
     public GUI(ArrayList<BenchmarkSorts.Results> bmsR) {
         System.out.println("GUI: Creating GUI");
         JFrame frame = new JFrame();
-        frame.setSize(600, 600);
+        frame.setSize(485, 220);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel(new FlowLayout());
 
         JTable table = createTable(bmsR);
-        table.setPreferredScrollableViewportSize(new Dimension(500, 50));
+        table.setPreferredScrollableViewportSize(new Dimension(470, 160));
         table.setFillsViewportHeight(true);
 
         JScrollPane scrollPane = new JScrollPane(table);
-        panel.add(scrollPane);
 
-        panel.add(table);
+        panel.add(scrollPane);
 
         frame.add(panel);
         frame.setVisible(true);
@@ -33,21 +33,22 @@ public class GUI {
         System.out.println("GUI: Creating table for GUI");
         String[] columnNames = { "Size", "Avg Count", "Ceof Count", "Avg Time", "Coef Time" };
 
+        DefaultTableModel defaultTableModel = new DefaultTableModel(r.size(), columnNames.length);
+        defaultTableModel.setColumnIdentifiers(columnNames);
+
         Object[][] data = new String[r.size()][columnNames.length];
 
         for (int i = 0; i < r.size(); i++) {
-            String[] temp = new String[]{
+            String[] temp = {
                     r.get(i).getSize(),
                     String.valueOf(r.get(i).getAvgCnt()),
                     String.valueOf(r.get(i).getCoefCnt()),
                     String.valueOf(r.get(i).getAvgTime()),
                     String.valueOf(r.get(i).getCoefTime())
             };
-            data[i][0] = temp;
+            data[i] = temp;
         }
 
         return new JTable(data, columnNames);
     }
-
-
 }
